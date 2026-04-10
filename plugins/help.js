c4f2ebab000ee03a5b36f22e1a2396cb0433dad5
Mohammad Ariyan <mdsohaghasan55663@gmail.com>
2026-04-10 13:52:25 +0600

    Update help.js

--- plugins/help.js
+++ plugins/help.js
@@ -111,8 +111,8 @@ module.exports = {
     }
 
     responseText += `
-╰──────────────────────╯`;
-
+╰──────────────────────╯
+✨ Power ARIYAN-BHAI`;
     try {
       const response = await axios.get(global.config.helpPic, { responseType: 'stream' });
       await api.sendMessage(threadId, {
